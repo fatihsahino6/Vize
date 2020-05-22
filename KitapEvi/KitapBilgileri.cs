3602f936527c31f6312be82d7b01d0c1a9ff45e4
Mehmet Fatih ŞAHİN <mehmetfatih104@gmail.com>
2020-05-22 19:32:23 +0300

    Proje dosyası ekle.

---
+++ KitapEvi/KitapBilgileri.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitapEvi
+{
+    class KitapBilgileri
+    {
+        public  string kitapadi;
+        string yazar;
+        DateTime btarihi;
+        string kturu;
+        public void SetKitapAdi(string kitapadi)
+        {
+            this.kitapadi = kitapadi;
+        }
+        public string GetKitapAdi()
+        {
+            return this.kitapadi;
+        }
+        public void SetYazar(string yazar)
+        {
+            this.yazar = yazar;
+        }
+        public string GetYazar()
+        {
+            return this.yazar;
+        }
+        public void SetBTarihi(DateTime btarihi)
+        {
+            this.btarihi = btarihi;
+        }
+        public DateTime GetBTarihi()
+        {
+            return this.btarihi;
+        }
+        public void SetKTuru(string kturu)
+        {
+            this.kturu = kturu;
+        }
+        public string GetKTuru()
+        {
+            return this.kturu;
+        }
+        public void Dongu()
+        {
+            FileStream fs = new FileStream(@"D:\KitapBilgileri.txt", FileMode.Append);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.Write("--------------------\n");
+            Console.Write("Kitap Adı : ");
+            sw.Write("Kitap Adı : " + (kitapbilgileri[satir, sutun] = Console.ReadLine()) + "\n");
+            Console.Write("Kitabın Yazarı : ");
+            sw.Write("Kitap Yazarı : " + (kitapbilgileri[satir, sutun] = Console.ReadLine()) + "\n");
+            Console.Write("Kitabın Basım Tarihi : ");
+            sw.Write("Kitap Basım Tarihi : " + (kitapbilgileri[satir, sutun] = Console.ReadLine()) + "\n");
+            Console.Write("Kitabın Türü : ");
+            sw.Write("Kitabın Türü : " + (kitapbilgileri[satir, sutun] = Console.ReadLine()) + "\n");
+            fs.Flush();
+            sw.Close();
+            fs.Close();
+        }
+        public string KitapBilgileriGetir() => $"Kitap Adi:{this.kitapadi}\nKitap Yazarı:{this.yazar}\nBasım Tarihi:{this.btarihi}\nKitap Türü:{this.kturu}";
+
+       }
+
+       
+
+    }
+
---
+++ KitapEvi/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitapEvi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte secim;
+            Console.WriteLine("1 - Kitap Ekleme işlemi\n2 - Kitapevindeki kitapların listesini görüntüleme");
+            secim = byte.Parse(Console.ReadLine());
+            if (secim == 1)
+            {
+                Console.Write("Kaç kitap eklemek istersiniz : ");
+                byte kitapsayisi = byte.Parse(Console.ReadLine());
+                string[,] kitapbilgileri = new string[kitapsayisi,5];
+                KitapBilgileri kitap = new KitapBilgileri();
+                
+                
+                    for (int satir = 0; satir <=kitapbilgileri.GetLength(0); satir++)
+                    {
+                  
+                        Console.WriteLine(satir + 1 + " . Kitap Bilgileri");
+
+                        for (int sutun = 0; sutun < kitapbilgileri.GetLength(1); sutun++)
+                        {
+                        
+                        break;
+                        }
+                }
+            }
+            else if(secim==2)
+            {
+                StreamReader sr = new StreamReader("D:\\KitapBilgileri.txt");
+                Console.WriteLine(sr.ReadToEnd());
+
+            }
+
+            
+
+            Console.ReadKey();
+
+        }
+       
+    }
+
+}
